21d678f63a4b64b7c7e9dde19d66930b44943c93
Dudda-soares <eduarda.soaresdsantos@gmail.com>
2025-06-06 20:05:33 -0300

    ok

--- dist/Aula-mod21.f75de5e1.js
+++ dist/Aula-mod21.f75de5e1.js
@@ -1,3 +1,26 @@
-console.log("Ol\xe1 Mundo");
+AOS.init();
+const dataDoEvento = new Date("Jul 10, 2025 19:00:00");
+const timeStampDoEvento = dataDoEvento.getTime();
+const contaHoras = setInterval(function() {
+    const agora = new Date();
+    const timeStampAtual = agora.getTime();
+    const distanciaAteOEvento = timeStampDoEvento - timeStampAtual;
+    const diasEmMs = 86400000;
+    const horaEmMS = 3600000;
+    const minEmMs = 60000;
+    const diasAteEvento = Math.floor(distanciaAteOEvento / diasEmMs);
+    const horaEvento = Math.floor(distanciaAteOEvento % diasEmMs / horaEmMS);
+    const minEvento = Math.floor(distanciaAteOEvento % horaEmMS / minEmMs);
+    const segEvento = Math.floor(distanciaAteOEvento % minEmMs / 1000);
+    /*console.log(diasAteEvento);
+    console.log(horaEvento);
+    console.log(minEvento);
+    console.log(segEvento);*/ const constador = document.getElementById('contador').innerHTML = `${diasAteEvento}d ${horaEvento}h ${minEvento}m ${segEvento}s`;
+    console.log(constador);
+    if (distanciaAteOEvento < 0) {
+        clearInterval(contaHoras);
+        document.getElementById('contador').innerHTML = `Evento indisponivel`;
+    }
+}, 1000);
 
 //# sourceMappingURL=Aula-mod21.f75de5e1.js.map
--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,1 +1,42 @@
-console.log('Olá Mundo');
+AOS.init();
+
+const dataDoEvento = new Date("Jul 10, 2025 19:00:00");
+const timeStampDoEvento = dataDoEvento.getTime();
+
+const contaHoras = setInterval(function(){
+    const agora = new Date();
+    const timeStampAtual = agora.getTime();
+
+
+    const distanciaAteOEvento = timeStampDoEvento - timeStampAtual ;
+
+    const diasEmMs = 1000 * 60 * 60 * 24;
+    const horaEmMS = 1000 * 60 * 60
+    const minEmMs = 1000 * 60  ;
+
+
+    const diasAteEvento = Math.floor(distanciaAteOEvento / (diasEmMs))
+
+    const horaEvento = Math.floor(distanciaAteOEvento % (diasEmMs)  / (horaEmMS) )
+
+    const minEvento = Math.floor(distanciaAteOEvento % (horaEmMS ) / (minEmMs))
+
+    const segEvento = Math.floor(distanciaAteOEvento % (minEmMs) / 1000)
+
+    /*console.log(diasAteEvento);
+    console.log(horaEvento);
+    console.log(minEvento);
+    console.log(segEvento);*/
+
+    const constador = document.getElementById('contador').innerHTML =`${diasAteEvento}d ${horaEvento}h ${minEvento}m ${segEvento}s`
+    
+    
+   console.log(constador);
+
+   if(distanciaAteOEvento <  0){
+        clearInterval(contaHoras);
+        document.getElementById('contador').innerHTML =`Evento indisponivel`;
+   }
+   
+
+},1000)
